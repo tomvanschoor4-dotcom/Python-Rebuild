b1bcf152136d60418247b15479c643347d3c1c1f
Tom Van Schoor <tomvanschoor4@gmail.com>
2026-04-02 14:55:07 -0700

    Cleaned the shape of the df

--- mini_projects/inventory_analysis/src/analysis.py
+++ mini_projects/inventory_analysis/src/analysis.py
@@ -3,11 +3,20 @@ import pandas as pd
 # load data
 df = pd.read_excel("../data/914_fiscal_recap_data_dump.xlsx")
 
-# preview
+# quick preview
+print("Original shape:",df.shape)
 print(df.head())
+# remove rows where key fields are missing
+df=df.dropna(subset=["Dept","SKU","Item"])
+# drop fully useless column
+df = df.drop(columns=["Metrics"],errors="ignore")
+# optional ; reset index after cleaning
+df = df.reset_index(drop=True)
 
+print("\nCleaned Shape:",df.shape)
+print(df.head())
 # columns
 print(df.columns)
-
+print(df.info())
 # info
 print(df.info())
