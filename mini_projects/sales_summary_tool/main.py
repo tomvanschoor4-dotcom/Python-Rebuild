be47e2068efe5076d7fce77a46cd7484853a15f4
Tom Van Schoor <tomvanschoor4@gmail.com>
2026-04-02 13:45:17 -0700

    add README and polish sales summary tool

--- mini_projects/sales_summary_tool/main.py
+++ mini_projects/sales_summary_tool/main.py
@@ -3,13 +3,23 @@ import pandas as pd
 # Load data
 df=pd.read_csv("sales.csv")
 # print(df)
-# Create total column
+# Create total revenue column
 df["total"] = df["price"] * df["quantity"]
-# Group by product
-summary = df.groupby("product", as_index=False)["total"].sum()
+# Build summary table
+summary = df.groupby("product", as_index=False).agg({
+    "total": "sum",
+    "quantity": "sum"
+})
+# renaming columns
+summary = summary.rename(columns={
+    "total":"Revenue",
+    "quantity":"Units Sold"
+}) 
 # sort highest to lowest
-summary = summary.sort_values(by="total",ascending=False)
-# output
+summary = summary.sort_values(by="Revenue",ascending=False)
+# Print output to terminal
 print("\nSales Summary:\n")
 print(summary)
-
+# Exporting to CSV
+summary.to_csv("sales_summary.csv",index=False)
+print("\nSaved file : sales_summary.csv")
