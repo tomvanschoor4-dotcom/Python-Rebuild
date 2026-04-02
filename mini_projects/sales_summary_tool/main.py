11d40d6e40d4ae24adfab9af65bc05dde5498e40
Tom Van Schoor <tomvanschoor4@gmail.com>
2026-04-02 13:26:52 -0700

    added a mini project called : sales summary tool

--- mini_projects/sales_summary_tool/main.py
+++ mini_projects/sales_summary_tool/main.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 # Load data
 df=pd.read_csv("sales.csv")
-print(df)
+# print(df)
 # Create total column
-
+df["total"] = df["price"] * df["quantity"]
+# Group by product
+summary = df.groupby("product", as_index=False)["total"].sum()
+# sort highest to lowest
+summary = summary.sort_values(by="total",ascending=False)
+# output
+print("\nSales Summary:\n")
+print(summary)
 
